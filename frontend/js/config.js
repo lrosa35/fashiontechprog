a41a2e833f22e65f372c4f2505fec6bf1d315688
lrosa35 <no-reply@example.com>
2025-10-22 16:18:19 -0300

    chore: apontar produção para Heroku e auto-config API_BASE

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,5 @@
-// Local development API endpoint
-window.API_BASE = "http://localhost:8090";
-
+// Auto-pick API endpoint based on host
+(function(){
+  var isLocal = /^(localhost|127\.0\.0\.1)$/i.test(window.location.hostname);
+  window.API_BASE = isLocal ? 'http://localhost:8090' : 'https://fashiontechprog-255445129a01\.herokuapp\.com';
+})();
